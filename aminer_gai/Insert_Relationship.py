fd6898887669fa6d9fab431fe0b7dfea4c3252e0
Jingfei Han <749846857@qq.com>
2017-06-22 23:08:33 +0800

    Update

--- aminer_gai/Insert_Relationship.py
+++ aminer_gai/Insert_Relationship.py
@@ -22,28 +22,23 @@ f = open("/home/jingfei/AMiner/AMiner-Paper.txt", "r")
 cur_index = 1
 citation_list = [] #设置引用列表为空
 while True:
-	
-	line = f.readline()
-	if line:
-		if line == '\n': #Insert data into the table
-
-            #记录引用量
-            #length = len(citation_list)
-            #将length写入对应论文的paper_nbCitation字段
-            sql_citation = "UPDATE paper SET paper_nbCitation='%d' WHERE paper_id= '%d'" %(length, relationship_dst)
-            try:
-                cursor.execute(sql_citation)
-				db.commit()
-			except:
-				sys.exit("Update failed!")
-
-			for relationship_src in citation_list:
-				sql1 = "INSERT INTO relationship(relationship_src, relationship_dst)\
+    line = f.readline()
+    if line:
+        if line == '\n':
+            #sql_citation = "UPDATE paper SET paper_nbCitation='%d' WHERE paper_id= '%d'" %(length, relationship_dst)
+            #try:
+#               cursor.execute(sql_citation)
+#               db.commit()
+#           except:
+#               sys.exit("Update failed!")
+            
+            for relationship_src in citation_list:
+                sql1 = "INSERT INTO relationship(relationship_src, relationship_dst)\
 						VALUES('%d', '%d')" %(relationship_src, relationship_dst)
-				try:
-					cursor.execute(sql1)
-					db.commit()
-				except:
+                try:
+                    cursor.execute(sql1)
+                    db.commit()
+                except:
 					#没有插入成功就不用管， 直接去掉就行
                     print "The realtionship is Failed!"
             
@@ -74,22 +69,21 @@ while True:
             """
 			#将引用列表赋为空
             citation_list = []
+            
+            if cur_index%1000 == 0:
+                print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), " The %dth relationship is INSERTED successfuly!" %cur_index
+            cur_index += 1
+            continue
 
+        elif line[1] == 'i':
+            relationship_dst = int(line.replace('#index', '').strip())
+        
+        elif line[1] == '%':
+            citation_list.append(int(line.replace('#%', '').strip())) #int型List
 
-			if cur_index%1000 == 0:
-				print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), " The %dth relationship is INSERTED successfuly!" %cur_index
-			cur_index += 1
-			continue
-
-		elif line[1] == 'i':
-			relationship_dst = int(line.replace('#index', '').strip())
+        #Add CCF information
 		
-		elif line[1] == '%':
-			citation_list.append(int(line.replace('#%', '').strip())) #int型List
-
-		#Add CCF information
-		
-	else: #END OF FILE
-		print "The %d relationships are INSERTED FINISHED!" %(cur_index)
-		break
+    else: #END OF FILE
+        print "The %d relationships are INSERTED FINISHED!" %(cur_index)
+        break
 f.close()
